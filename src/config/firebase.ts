8e614a4e5bb25f1e79b9993b222a4d5fc8448067
Jayden <jayden8611@gmail.com>
2025-05-29 01:06:25 +0800

    feat: adding verify id token middleware

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,5 +1,10 @@
 import { initializeApp , applicationDefault } from 'firebase-admin/app';
+import { getAuth } from 'firebase-admin/auth';
+import { getFirestore } from 'firebase-admin/firestore';
 
 const app = initializeApp({
     credential: applicationDefault(),
 });
+
+export const auth = getAuth(app);
+export const firestore = getFirestore(app);
