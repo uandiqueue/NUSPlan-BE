ef3e80f790ea8a1d203b71702d3f8cbec281dc6f
Jayden <jayden8611@gmail.com>
2025-05-29 21:36:42 +0800

    feat: updated requirement-types with Elective types and added premature mock data

--- src/types/requirements-types.ts
+++ src/types/requirements-types.ts
@@ -1,48 +1,52 @@
-import type { Module } from "../types/nusmods-types";
+import type { ModuleCode } from "../types/nusmods-types";
 
-// Represents the requirements for core electives grouped by level.
-export interface CoreElectivesByLevel {
-    included?: boolean;
-    excluded?: Module[];
-    maxUnits?: number;
-    minUnits?: number;
+// Represents the requirements/constraints for electives
+export interface Elective {
+    name?: string; // name of the constraint, eg: "Level-2000 Core Electives"
+    type: "min" | "max"; // type of constraint, either minimum or maximum
+    value: number; // value of the constraint (in units)
+    modules?: ModuleCode[]; // optional — only present on leaf nodes
+    nested?: Elective[]; // optional — for branching constraints
     note?: string;
 }
 
 // Represents the requirements for a program, 
-// including core essentials, core electives, 
-// optional core specials (eg: industry/research attachment),
+// including common curriculum (faculty/university requirement) (not implemented yet),
+// core essentials, core electives, 
+// optional core specials (specialisations),
+// other requirements (eg: industrial attachment),
 // unrestricted electives, and some constraints
 export interface ProgramRequirement {
-    coreEssentials: Module[];
-    coreElectives: { [groupName: string]: CoreElectivesByLevel };
-    coreSpecials?: Module[];
-    unrestrictedElectives?: Module[];
+    coreEssentials: ModuleCode[];
+    coreElectives: Elective[];
+    coreSpecials?: Elective[];
+    coreOthers?: Elective[];
+    unrestrictedElectives?: Elective[];
     constraintModules?: {
-        // Contains all modyules that are overlapping with other programs,
+        // Contains all modules that are overlapping with other programs,
         // if double-count units cap is reached,
         // part of these modules will not be counted towards the program
-        doubleCountModules?: Module[];
+        doubleCountModules?: Elective[];
 
         // Contains all Level-1000 modules that can be counted towards the program
         // if level-1000 units cap is reached, 
         // part of these modules will not be counted towards the program
-        level1000Modules?: Module[];
+        level1000Modules?: Elective[];
         
         // Contains all Level-2000 modules that can be counted towards the program
         // if level-2000 units cap exists and is reached,
         // part of these modules will not be counted towards the program
-        level2000Modules?: Module[];
+        level2000Modules?: Elective[];
 
         // Contains all modules that are not NUS-taught,
         // if NUS-taught units cap is reached,
         // part of these modules will not be counted towards the program
-        nonNUSModules?: Module[];
+        nonNUSModules?: Elective[];
 
         // Only for minors:
         // Contains all modules that are listed in another program's course list,
         // if similar modules cap is reached,
         // part of these modules will not be counted towards the program
-        nonUniqueModules?: Module[];
+        nonUniqueModules?: Elective[];
     };
 }
