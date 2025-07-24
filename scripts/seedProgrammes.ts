e428b39b35580a7c2a8ca6ea6378f49de45b191f
Jayden <jayden8611@gmail.com>
2025-07-24 22:28:26 +0800

    feature: added more minors

--- scripts/seedProgrammes.ts
+++ scripts/seedProgrammes.ts
@@ -1,6 +1,6 @@
 // Run this script to seed a programme from a JSON file
 // Replace `fileName` with the `type/name.json` to be seeded
-let fileName = "major/Business Analytics.json"
+let fileName = "minor/Artificial Intelligence.json"
 // Usage: 
 // 1. Replace fileName
 // 2. ts-node scripts/seedProgrammes.ts
